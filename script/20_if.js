13238bc423cc4fc8642a4d22192028f688b209d5
chae-jung <126949555+chae-jung@users.noreply.github.com>
2023-05-12 06:27:47 +0900

    Create 20_if.js

---
+++ script/20_if.js
@@ -0,0 +1,34 @@
+var day = prompt("영어로 요일을 입력하세요");
+
+if(day == "Monday")
+{
+    alert("월요일");
+}
+else if(day == "Tuesday")
+{
+    alert("화요일");
+}
+else if(day == "Wednesday")
+{
+    alert("수요일");
+}
+else if(day == "Thursday")
+{
+    alert("목요일");
+}
+else if(day == "Friday")
+{
+    alert("금요일");
+}
+else if(day == "Saturday")
+{
+    alert("토요일")
+}
+else if(day == "Sunday")
+{
+    alert("일요일");
+}
+else
+{
+    alert("알 수 없는 입력 값");
+}
